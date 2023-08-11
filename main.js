c503c5d4ad661931e5bfcfea2325f758d6012df2
Marcos Giannasio <marcosgiannasio@gmail.com>
2023-08-11 20:52:22 -0300

    cambios

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function miAlerta(evt){
 // Evitar reload de pagina
  
 
-/* let urlPages ;
+let urlPages ;
 
 
 const links = document.querySelectorAll('#pages')
@@ -134,4 +134,3 @@ function pedirPagina(urlPages) {
         alert(err);
       })
 }
- */
